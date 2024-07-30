4034efa567dc3293cecb5bf406c2db53538aa82c
DrakeFruit <94070205+DrakeFruit@users.noreply.github.com>
2024-07-30 13:53:38 -0700

    upload whatever this is

--- code/Chat.cs
+++ code/Chat.cs
@@ -9,13 +9,15 @@ using System.Threading.Tasks;
 [Icon( "electrical_services" )]
 public sealed class Chat : Component
 {
+    [Property] public String system_prompt { get; set; } = "You are a helpful AI assistant.";
+
     public AIAssistant assistant = new AIAssistant();
     public String completion;
-    public async Task SendInput( string prompt )
+    public async Task SendInput( string prompt, string system_prompt )
     {
         Log.Info("sending prompt: " + prompt);
-        completion = await assistant.Complete( prompt );
-        Log.Info( completion );
+        completion = await assistant.Complete( prompt, system_prompt );
+        assistant.history.Add(completion);
     }
 }
 
@@ -23,8 +25,8 @@ public class AIAssistant
 {
     // This is the history of prompts and completions. This is used to give the AI context for the current prompt.
     public List<string> history;
-    public string system_prompt = "You are a very powerful AI assistant, your responses are short and sweet.";
-    private string Url = "https://iiced-mixtral-46-7b-fastapi.hf.space/generate/";
+    public string system_prompt;
+    private string Url = "https://drakefruit-mixtral-46-7b-fastapi.hf.space/generate/";
 
     // Constructor
     public AIAssistant()
@@ -32,21 +34,20 @@ public class AIAssistant
         history = new List<string>();
     }
 
-    public async Task<string> Complete( string prompt )
+    public async Task<string> Complete( string prompt, string system_prompt )
     {
         // Create HTTP Content. This is the json object that is sent in the body of the request.
         var content = new StringContent( JsonSerializer.Serialize( new
         {
             prompt,
             history,
-            system_prompt
+			system_prompt
         } ), Encoding.UTF8, "application/json" );
         try
         {
             // Make the request to the API
             var response = await Http.RequestAsync(Url, "POST", content);
-            // TODO : Add both the prompt and the completion to the history and limit the history to X amount of entries. This will allow the following prompt to have context.
-
+            
             // Return the response from the API. You probably want to parse this and only return the completion, as this is a json string.
             return await response.Content.ReadAsStringAsync();
         }
